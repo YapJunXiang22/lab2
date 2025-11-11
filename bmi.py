4d0b74bad88fa177b41b21acdd00958bac5d9a65
YapJunXiang <JXJX22.23@ichat.sp.edu.sg>
2025-11-11 16:25:36 +0800

    create bmi file

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height**2)
+    print ("BMI = ", f"{bmi: .2f}")
+
+    if bmi < 18.5:
+        print("under weight")
+    elif bmi >25.0:
+        print("over weight")
+    else:
+        print("normal weight")
+
+calculate_bmi(weight=57, height=1.73)
