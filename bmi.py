5825585c031ca6abcded39b0fc994d49e316a51f
YapJunXiang <JXJX22.23@ichat.sp.edu.sg>
2025-11-13 10:11:41 +0800

    updated bmi

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("under weight")
+        return -1
     elif bmi >25.0:
         print("over weight")
+        return 1
     else:
         print("normal weight")
+        return 0
 
 calculate_bmi(weight=57, height=1.73)
